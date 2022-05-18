48c363f6153744b8626f05167a7b7c43927e5c9f
Yasin Aydin <yasinaydin@sabanciuniv.edu>
2022-05-18 18:57:04 +0300

    Fix keccak hash

--- merkle_tree.js
+++ merkle_tree.js
@@ -11,16 +11,21 @@ let whitelistAddresses = [
 	'0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2',
 	'0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db',
 	'0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB',
-	'0x617F2E2fD72FD9D5503197092aC168c91465E7f2', // The sample address from remix
+	'0x617F2E2fD72FD9D5503197092aC168c91465E7f2',
+	'0xAb3A4965C5A6e1b317659D90034f60aD3b29cBA7', // The sample address from remix
 ];
 
 // 3. Create a new array of `leafNodes` by hashing all indexes of the `whitelistAddresses`
 // using `keccak256`. Then creates a Merkle Tree object using keccak256 as the algorithm.
 //
 // The leaves, merkleTree, and rootHas are all PRE-DETERMINED prior to whitelist claim
-const leafNodes = whitelistAddresses.map((addr) =>
-	keccak256(Buffer.from(addr))
-);
+const leafNodes = whitelistAddresses.map((addr) => keccak256(addr));
+
+// To get the real keccak hash we should use this but this isn't working for merkle root.
+// for the matching of different libraries we can use this and compare
+//const leafNodes = whitelistAddresses.map((addr) =>
+//	keccak256(Buffer.from(addr))
+//);
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
 
 // 4. Get root hash of the `merkleeTree` in hexadecimal format (0x)
@@ -38,7 +43,7 @@ console.log(
 // required to derive the root hash of the Merkle Tree
 
 // ✅ Positive verification of address
-const claimingAddress = leafNodes[1];
+const claimingAddress = leafNodes[5];
 // ❌ Change this address to get a `false` verification
 // const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
 
