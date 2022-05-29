98b7edf1ea29a43f69d02cd905874a0201e8d883
Yasin Aydin <yasinaydin@sabanciuniv.edu>
2022-05-29 16:04:23 +0300

    Add prooflist print

--- merkle_tree.js
+++ merkle_tree.js
@@ -7,12 +7,10 @@ const keccak256 = require('keccak256');
 
 // 2. Collect list of wallet addresses from competition, raffle, etc.
 let whitelistAddresses = [
-	'0x5B38Da6a701c568545dCfcB03FcB875f56beddC4',
-	'0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2',
-	'0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db',
-	'0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB',
-	'0x617F2E2fD72FD9D5503197092aC168c91465E7f2',
-	'0xAb3A4965C5A6e1b317659D90034f60aD3b29cBA7', // The sample address from remix
+	'0xAb3A4965C5A6e1b317659D90034f60aD3b29cBA7',
+	'0x77CfcAd27e4A4b25eBD54808DC3692E4AcD9541D',
+	'0x27AC1C615ee9102D7847BC611436945c8260D1BC',
+	'0x909f741CfE1eBcc84a1DF973A4B2E01011f79792', // The sample address from remix
 ];
 
 // 3. Create a new array of `leafNodes` by hashing all indexes of the `whitelistAddresses`
@@ -20,7 +18,18 @@ let whitelistAddresses = [
 //
 // The leaves, merkleTree, and rootHas are all PRE-DETERMINED prior to whitelist claim
 const leafNodes = whitelistAddresses.map((addr) => keccak256(addr));
-
+// console.log(leafNodes)
+// console.log(
+// 		Buffer.from(
+// 			keccak256(
+// 				Buffer.from(
+// 					'0xE83af0De0111eB65ea0C67244333b851e781065f',
+// 					'hex'
+// 				).toString('hex')
+// 			),
+// 			'hex'
+// 		).toString('hex')
+// );
 // To get the real keccak hash we should use this but this isn't working for merkle root.
 // for the matching of different libraries we can use this and compare
 //const leafNodes = whitelistAddresses.map((addr) =>
@@ -43,15 +52,37 @@ console.log(
 // required to derive the root hash of the Merkle Tree
 
 // ✅ Positive verification of address
-const claimingAddress = leafNodes[5];
+// const claimingAddress = leafNodes[1];
 // ❌ Change this address to get a `false` verification
 // const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
 
 // `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
 // be required to derive the Merkle Trees root hash.
-const hexProof = merkleTree.getHexProof(claimingAddress);
-console.log(JSON.stringify(hexProof));
+// const hexProof = merkleTree.getHexProof(claimingAddress);
+// console.log(JSON.stringify(hexProof));
+console.log('----------------For Dart-----------------');
+// console.log(Buffer.from(leafNodes[0], 'hex').toString('hex'));
+let proofs = [];
+leafNodes.forEach((element) => {
+	let hexProof =
+		'[hex.decode("' +
+		merkleTree
+			.getHexProof(element)
+			.map((hex) => hex.substring(2))
+			.join('"), hex.decode("') +
+		'")]';
+	proofs.push(hexProof);
+});
+console.log(proofs);
+
+console.log('-------------------Solidity/javascript-----------------------');
+let proofs2 = [];
+leafNodes.forEach((element) => {
+	let hexProof = merkleTree.getHexProof(element);
+	proofs2.push(JSON.stringify(hexProof));
+});
+console.log(proofs2);
 
 // ✅ - ❌: Verify is claiming address is in the merkle tree or not.
 // This would be implemented in your Solidity Smart Contract
-console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));
+// console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));
